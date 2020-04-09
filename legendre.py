04b2782f6e74b0a9f46844dba73234c2c4bb6f64
Pedro Rangel Caetano <p.r.caetano@gmail.com>
2020-04-09 15:19:24 -0300

    Allows more flexible polynomial corrections.

--- legendre.py
+++ legendre.py
@@ -7,12 +7,22 @@ from names import bao_par_section, syst_par_section, bao_data_section
 
 def setup(options):
     max_multipole = options.get_int(option_section, "max_multipole", 2)
+    min_k_power = options.get_int(option_section, "min_shape_k_power", 0)
+    max_k_power = options.get_int(option_section, "max_shape_k_power", 2)
     multipoles = np.arange(0, max_multipole+2, 2)
-    return multipoles
+
+    data = {
+            "multipoles": multipoles,
+            "min_k_power": min_k_power,
+            "max_k_power": max_k_power,
+           }
+    return data
 
 
 def execute(block, config):
-    multipoles = config
+    multipoles = config["multipoles"]
+    min_k_power = config["min_k_power"]
+    max_k_power = config["max_k_power"]
 
     k = block[bao_data_section, "k"]
     mu = block[bao_data_section, "mu"]
@@ -23,10 +33,15 @@ def execute(block, config):
     pk_multipoles = (2*multipoles.reshape(-1, 1) + 1) * trapz(integrands, x=mu, axis=-1)
 
     for l, pk_multipole in enumerate(pk_multipoles):
+        powers = np.arange(min_k_power, max_k_power+1)
+        a_s = [block[syst_par_section, "a{}_l{}".format(power, 2*l)] for power in powers]
+        syst_correction = np.zeros_like(k, dtype=float)
+        for power, a in zip(powers, a_s):
+            syst_correction +=  a * k**power
         a0 = block[syst_par_section, "a0_l{}".format(2*l)]
         a1 = block[syst_par_section, "a1_l{}".format(2*l)]
         a2 = block[syst_par_section, "a2_l{}".format(2*l)]
-        syst_correction = a0 + a1*k + a2*k**2
+
         pk_multipole += syst_correction.flatten()
         pk_multipoles[l] = pk_multipole
 

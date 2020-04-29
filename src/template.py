e4004c63bdbad950226619c89e6258e71994271e
Pedro Rangel Caetano <p.r.caetano@gmail.com>
2020-04-29 03:48:04 -0300

    Adds option to use boltzmann rs_zdrag and k_equality

--- src/template.py
+++ src/template.py
@@ -30,13 +30,16 @@ def setup(options):
         print("No no_wiggle_template_fname passed. I'll compute an Eisenstein&Hu "
               "template at the given cosmology, with an polynomial broadband "
               "term fitted together.", file=sys.stderr)
+        use_eh_sound_horizon = options.get_bool(option_section, "use_eh_sound_horizon", True)
+        use_eh_k_eq = options.get_bool(option_section, "use_eh_k_eq", True)
         pmin = options.get_int(option_section, "min_power_broadband_polynomial", 0)
         pmax = options.get_int(option_section, "max_power_broadband_polynomial", 0)
 
     # Save variables to be used during execute
     vars_to_save = ["loaded_bao_template", "loaded_no_wiggle_template", "k_bao", "pk_bao", "z",
                     "output_bao_template_fname", "k_nw", "pk_nw",
-                    "output_no_wiggle_template_fname", "pmin", "pmax"]
+                    "output_no_wiggle_template_fname", "pmin", "pmax",
+                    "use_eh_sound_horizon", "use_eh_k_eq"]
     loaded_data = dict()
     for key in vars_to_save:
         if key in locals():
@@ -70,6 +73,11 @@ def execute(block, config):
         pmin = config["pmin"]
     if "pmax" in config:
         pmax = config["pmax"]
+    if "use_eh_sound_horizon" in config:
+        use_eh_sound_horizon = config["use_eh_sound_horizon"]
+    if "use_eh_k_eq" in config:
+        use_eh_k_eq = config["use_eh_k_eq"]
+
 
     if not loaded_bao_template:
         k_bao = block[names.matter_power_lin, "k_h"]
@@ -92,10 +100,21 @@ def execute(block, config):
 
     if not loaded_no_wiggle_template:
         k_nw = k_bao
+        if use_eh_sound_horizon:
+            s = None
+        else:
+            print("Using sound horizon computed by boltzmann solver.", file=sys.stderr)
+            s = block[names.distances, "RS_ZDRAG"]
+        if use_eh_k_eq:
+            k_eq = None
+        else:
+            print("Using k_eq computed by boltzmann solver.", file=sys.stderr)
+            k_eq = block[names.distances, "K_EQUALITY"]
         pk_nw = eisenstein_hu(k_nw, h=block[names.cosmological_parameters, "h0"],
                               Om=block[names.cosmological_parameters, "omega_m"],
                               Ob=block[names.cosmological_parameters, "omega_b"],
-                              ns=block[names.cosmological_parameters, "n_s"])
+                              ns=block[names.cosmological_parameters, "n_s"],
+                              s=s, k_eq=k_eq)
         # Normalizing pk_nw scale to pk_bao minimizing the residue (bao)
         #delta_k = np.ones_like(k_nw)
         #delta_k[:-1] = k_nw[1:] - k_nw[:-1]
@@ -112,7 +131,7 @@ def execute(block, config):
     return 0
 
 
-def eisenstein_hu(k, h, Om, Ob, ns, s=None, Tcmb0=2.7255):
+def eisenstein_hu(k, h, Om, Ob, ns, s=None, k_eq=None, Tcmb0=2.7255):
     """
     Computes Eisenstein and Hu no-wiggle power spectrum.
 
@@ -122,7 +141,9 @@ def eisenstein_hu(k, h, Om, Ob, ns, s=None, Tcmb0=2.7255):
         Om (float): Omega_matter today
         Ob (float): Omega_baryons today
         ns (float): primordial power spectrum spectral index
-        s (float or None): sound horizon at z_drag, in Mpc/h (if None uses E&H formula)
+        s (float or None): sound horizon at z_drag, in Mpc (if None uses E&H formula)
+        k_eq (float or None): k of matter-radiation equality, in Mpc
+                              (if None uses E&H formula)
         Tcmb0 (float): CMB temperature today (defaults to 2.7255)
 
     Returns:
@@ -138,12 +159,13 @@ def eisenstein_hu(k, h, Om, Ob, ns, s=None, Tcmb0=2.7255):
     k = np.sort(k)
 
     # wavenumber of equality
-    k_eq = 0.0746 * Omh2 * theta_cmb ** (-2) # units of 1/Mpc
+    if k_eq is None:
+        k_eq = 0.0746 * Omh2 * theta_cmb ** (-2) # units of 1/Mpc
 
     if s is None:
         sound_horizon = h * 44.5 * np.log(9.83/Omh2) / np.sqrt(1 + 10 * Obh2** 0.75) # in Mpc/h
     else:
-        sound_horizon = s
+        sound_horizon = s * h
     alpha_gamma = 1 - 0.328 * np.log(431*Omh2) * f_baryon + 0.38 * np.log(22.3*Omh2) * f_baryon ** 2
 
     k = k * h
